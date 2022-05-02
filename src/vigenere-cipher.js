50c3902cf02bdffadbe687e1ae7668d7d3270912
Alexandra Spirina <aleksandra.spirina.link@gmail.com>
2022-05-02 20:03:23 +0300

    feat: complete vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -23,22 +23,27 @@ class VigenereCipheringMachine {
 
 	constructor(...args) {
 		this.type = (args.length === 0 || args[0] === true) ? "direct" : "reverse";
+		this.startRegister = '';
 	}
 
 	encrypt(...args) {
-		if (args.length < 2) {
+		if (args.length < 2 || !args[0] || !args[1]) {
 			throw new Error("Incorrect arguments!");
 		}
+
+		this.startRegister = (args[0] === args[0].toUpperCase()) ? 'upper' : 'lower';
+
 		let message = args[0].toUpperCase();
 		let key = args[1].toUpperCase();
 
 		let counter = 0;
+		const START = 65;
 		let arr = message.split('');
+
 		arr.forEach((element, i, obj) => {
 			if (/[A-Z]/i.test(element)) {
 				let code = (element.charCodeAt(0) + key.charCodeAt(counter % key.length)) % 26;
-				obj[i] = String.fromCharCode(code);
-				console.log(obj[i]);
+				obj[i] = String.fromCharCode(START + code);
 				++counter;
 			}
 		});
@@ -46,12 +51,30 @@ class VigenereCipheringMachine {
 		arr = (this.type === "reverse") ? arr.reverse() : arr;
 		return arr.join('');
 	}
+
 	decrypt(...args) {
-		if (args.length < 2) {
+		if (args.length < 2 || !args[0] || !args[1]) {
 			throw new Error("Incorrect arguments!");
 		}
-		let message = args[0];
-		let key = args[1];
+
+		let message = args[0].toUpperCase();
+		let key = args[1].toUpperCase();
+
+		let counter = 0;
+		const START = 65;
+		const END = 90;
+
+		let arr = message.split('');
+		arr.forEach((element, i, obj) => {
+			if (/[A-Z]/i.test(element)) {
+				let code = (element.charCodeAt(0) - key.charCodeAt(counter % key.length)) % 26;
+				obj[i] = (code >= 0) ? String.fromCharCode(START + code) : String.fromCharCode(END + code + 1); 
+				++counter;
+			}
+		});
+
+		arr = (this.type === "reverse") ? arr.reverse() : arr;
+		return (this.startRegister === 'upper' || this.startRegister === '') ? arr.join('').toUpperCase() : arr.join('').toLowerCase();
 	}
 }
 
